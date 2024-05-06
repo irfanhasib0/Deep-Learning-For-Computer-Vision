d03299fbc389be1ef0d69ea120bb8dc4c1e71e2e
irfanhasib0 <irfanhasib.me@gmail.com>
2024-05-06 14:35:30 +0900

    cleaning up the repository

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
-export CUDA_HOME='/usr/local/cuda-11.4'
-cd detectron2/ && python3 setup.py build
-pip3 install cloudpickle
-pip3 install omegaconf
-cd ../fvcore/ && python3 setup.py install
-pip3 install cython
-cd ../cocoapi/PythonAPI/ && python3 setup.py install
-cd ../../
-#mv detectron2/build/lib.linux-x86_64-3.8/detectron2 ./build/...
+#!/bin/bash
+#sudo apt-get install python3.8 python3-pip
+#pip3 install virtualenv
+python3 -m virtualenv python_38
+source python_38/bin/activate
+#pip3 install -r requirements.txt
+#pip3 install torch==1.12.1+cu116 torchvision==0.13.1+cu116 torchaudio==0.12.1+cu116 -f https://download.pytorch.org/whl/cu116/torch_stable.html
+python3.8 -m pip install ninja
+python3.8 -m pip install -e detectron2 
+python3.8 -m pip install -e cocoapi/PythonAPI/pycocotools
+#sudo ./cuda.sh
+#sudo ln -s /usr/local/cuda-11.4/targets/x86_64-linux/lib/libcusolver.so.11 $PWD/python_38/lib/python3.8/site-packages/tensorflow/python/libcusolver.so.10
+#sudo ln -sfT /usr/local/cuda-11.4/bin/nvcc /usr/local/cuda/bin/nvcc
